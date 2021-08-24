37389860da62e43ccc991e0c5d4b96c2854394c4
Nikola Nenov <nikola.nenoff@gmail.com>
2021-08-24 21:22:04 +0300

    Added address property

--- src/CommunityAssociationManager/CommunityAssociationManager/Shared/Models/Property.cs
+++ src/CommunityAssociationManager/CommunityAssociationManager/Shared/Models/Property.cs
@@ -4,6 +4,7 @@
     {
         public long Id { get; set; }
         public CommunityMember Owner { get; set; }
+        public string Address { get; set; }
         public Tax Tax { get; set; }
     }
 }
