0c7eeb140a0c05fe237ded3cc41d2c2856aa4b9a
Nikola Nenov <nikola.nenoff@gmail.com>
2021-09-20 07:13:42 +0300

    Added repository interfaces

---
+++ src/CommunityAssociationManager/CommunityAssociationManager/Shared/Repositories/ICommunityMemberRepository.cs
@@ -0,0 +1,16 @@
+﻿using CommunityAssociationManager.Shared.Models;
+using System.Collections.Generic;
+
+namespace CommunityAssociationManager.Shared.Repositories
+{
+    internal interface ICommunityMemberRepository
+    {
+        CommunityMember GetCommunityMember(uint communityMemberId);
+        CommunityMember AddCommunityMember(CommunityMember communityMember);
+        CommunityMember UpdateCommnunityMember(CommunityMember communityMember);
+        void RemoveCommunityMember(uint communityMemberId);
+        IList<Property> GetCommunityMemberProperties();
+        IList<Community> GetManagedCommunities();
+        IList<Community> GetCashieredCommunities();
+    }
+}
