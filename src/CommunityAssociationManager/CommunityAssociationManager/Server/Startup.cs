dc6939e697926cf38635b96cc8088a5351c02f41
Nikola Nenov <nikola.nenoff@gmail.com>
2021-10-01 06:27:18 +0300

    Added service registration for DB classes

--- src/CommunityAssociationManager/CommunityAssociationManager/Server/Startup.cs
+++ src/CommunityAssociationManager/CommunityAssociationManager/Server/Startup.cs
@@ -39,6 +39,10 @@ namespace CommunityAssociationManager.Server
             services.AddAuthentication()
                 .AddIdentityServerJwt();
 
+            services.AddSingleton<IPropertyRepository, PropertyRepository>(); 
+            services.AddSingleton<ICommunityMemberRepository, CommunityMemberRepository>(); 
+            services.AddSingleton<ICommunityRepository, CommunityRepository>(); 
+            
             services.AddControllersWithViews();
             services.AddRazorPages();
         }
