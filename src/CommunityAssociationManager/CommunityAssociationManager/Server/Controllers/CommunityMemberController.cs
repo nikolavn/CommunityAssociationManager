45594b2ddef82343877134478b914afc7acef937
Nikola Nenov <nikola.nenoff@gmail.com>
2021-10-12 07:19:55 +0300

    Added actions to controller routes

--- src/CommunityAssociationManager/CommunityAssociationManager/Server/Controllers/CommunityMemberController.cs
+++ src/CommunityAssociationManager/CommunityAssociationManager/Server/Controllers/CommunityMemberController.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace CommunityAssociationManager.Server.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     [ApiController]
     public class CommunityMemberController : Controller
     {
